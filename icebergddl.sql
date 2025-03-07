b33230fd7651f056c4c709d1cbe4c9adaed61c43
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2025-03-07 13:41:53 -0500

    Create icebergddl.sql

---
+++ icebergddl.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE ICEBERG TABLE ICYTRANSCOME (
+	TITLE STRING,
+	DESCRIPTION STRING,
+	PUBDATE STRING,
+	POINT STRING,
+	LATITUDE STRING,
+	TS STRING,
+	LONGITUDE STRING,
+	UUID STRING NOT NULL,
+	primary key (UUID)
+  )
+  CATALOG = 'SNOWFLAKE'
+  EXTERNAL_VOLUME = 'transcom_tspanniceberg_extvol'
+  BASE_LOCATION = 's3://se-tspann-apacheiceberg/transcom/';
+
+
+  describe table ICYTRANSCOME;
+
+  select * from ICYTRANSCOME;
+
+describe external volume transcom_tspanniceberg_extvol;
+
+  CREATE OR REPLACE EXTERNAL VOLUME transcom_tspanniceberg_extvol
+   STORAGE_LOCATIONS =
+      (
+         (
+            NAME = 'se-tspann-apacheiceberg'
+            STORAGE_PROVIDER = 'S3'
+            STORAGE_BASE_URL = 's3://se-tspann-apacheiceberg/'
+            STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::343434:role/se_tspann_iceberg_table_nifirole'
+            STORAGE_AWS_EXTERNAL_ID = 'se_tspann_iceberg_table_external_id'
+         )
+      );
+
+# https://docs.snowflake.com/en/user-guide/tables-iceberg-configure-external-volume-s3
